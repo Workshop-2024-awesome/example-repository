b7f910a0a8a5d423ad7d614c0d19ba8a10219b94
Sebastian Walter <sebastian.walter@mantro.net>
2024-09-25 14:08:22 +0200

    Adds test config

--- server/src/config/app.config.ts
+++ server/src/config/app.config.ts
@@ -2,6 +2,7 @@ import { ConfigModule } from "@nestjs/config";
 
 export const AppConfiguration = ConfigModule.forRoot({
     envFilePath: [
+        '.env.test',
         '.env.local',
         './.env'],
     isGlobal: true
